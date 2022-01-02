190cb9eb79a2d72c748b03d949eaf9ff2b064285
Ashwin Chandran <ashwinchandran13@gmail.com>
2022-01-02 21:25:03 +0530

    fix: added service card

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,25 @@
 import { GetServerSidePropsContext, GetStaticPropsContext } from 'next';
-// import { services } from '../data';
+import { services } from '../data';
 
-const index = ({services}) => {
-  
-  
+import ServiceCard from '../components/serviceCard';
+
+const index = () => { 
   return (
     <div>
-      <h1>Hi world</h1>
+      <h5>I am currently working as a Software Developer
+        mainly focusing on frontend and backend development.
+        I have 1+ year experience in web development and beginner 
+        knowledge in system programming.
+      </h5>
+      <div>
+        <h6>What I Offer</h6>
+        <div>
+          {
+            // eslint-disable-next-line react/jsx-key
+            services.map((service) => <ServiceCard service={service}/>)
+          }
+        </div>
+      </div>
     </div>
   )
 }
@@ -28,18 +41,18 @@ export default index;
 
 // }
 
-export const getStaticProps = async (context:GetStaticPropsContext) => {
+// export const getStaticProps = async (context:GetStaticPropsContext) => {
 
-  // whatever happens here is returned as props
+//   // whatever happens here is returned as props
 
-  const res = await fetch('http://localhost:3000/api/services');
-  const data = await res.json();  
-  console.log('inside server');
+//   const res = await fetch('http://localhost:3000/api/services');
+//   const data = await res.json();  
+//   console.log('inside server');
   
-  return {
-    props:{
-      services: data.services,
-    }
-  }
+//   return {
+//     props:{
+//       services: data.services,
+//     }
+//   }
 
-}
+// }
