47ca5d04f9bff3e3b30e72ce63827e4a70b9b515
Ashwin Chandran <ashwinchandran13@gmail.com>
2022-01-16 22:57:21 +0530

    projects card added

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,7 +1,20 @@
+import ProjectCard from "../components/projectCard"
+import { projects } from "../data"
+
 const Projects = () => {
     return (
         <div className="p-4">
             <nav>Navbar</nav>
+
+            <div>
+                {
+                    projects.map((project) => {
+                        <div>
+                            <ProjectCard project={project} key={project.name}/>
+                        </div>
+                    })
+                }
+            </div>
         </div>
     )
 }
