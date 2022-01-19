c9f62b89b36d8771b5a9ec2b16f42013f6c02cc7
Ashwin Chandran <ashwinchandran13@gmail.com>
2022-01-19 22:43:49 +0530

    fix: projectcard ui changes

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -1,10 +1,12 @@
-import { FunctionComponent } from "react";
+import { FunctionComponent, useState } from "react";
+import { AiFillGithub, AiFillProject } from "react-icons/ai";
+import { MdClose } from "react-icons/md";
 import { IProject } from "../type";
 
-const ProjectCard:FunctionComponent<{
-    project:IProject;
+const ProjectCard: FunctionComponent<{
+    project: IProject;
 }> = ({
-    project:{
+    project: {
         name,
         image_path,
         category,
@@ -14,7 +16,52 @@ const ProjectCard:FunctionComponent<{
         key_techs
     }
 }) => {
-  return <div>{name}</div>;
-};
+
+        const [showDetail, setShowDetail] = useState(false);
+
+        return (
+            <div>
+                <img src={image_path} alt={name} className="cursor-pointer" onClick={() => setShowDetail(true)}/>
+                <p className="my-2 text-center">{name}</p>
+
+                {showDetail &&
+
+                    <div className="grid md:grid-cols-2">
+
+                        <div>
+                            <img src={image_path} alt={name} />
+                            <div>
+                                <a href={github_url}>
+                                    <AiFillGithub /><span>Github</span>
+                                </a>
+                                <a href={deployed_url}>
+                                    <AiFillProject /><span>Project</span>
+                                </a>
+                            </div>
+
+                        </div>
+
+                        <div>
+                            <h2>{name}</h2>
+                            <h3>{description}</h3>
+                            <div>
+                                {
+                                    key_techs.map(tech => <span key={tech}>
+                                        {tech}
+                                    </span>)
+                                }
+                            </div>
+                        </div>
+
+                        <button>
+                            <MdClose size={30} onClick={() => setShowDetail(false)}/>
+                        </button>
+
+                    </div>
+                }
+
+            </div>
+        );
+    };
 
 export default ProjectCard;
