2200e7a590cedb4d076df0f515a60b716de310c4
Ashwin Chandran <ashwinchandran13@gmail.com>
2022-01-21 21:03:59 +0530

    fix: added framer motion animations

--- pages/resume.tsx
+++ pages/resume.tsx
@@ -1,28 +1,32 @@
 import Bar from "../components/bar"
 import { languages, tools } from "../data"
+import { fadeInUp } from "../animations"
+
+import { motion } from "framer-motion"
 
 const Resume = () => {
+
     return (
         <div className="px-6 py-2">
             {/* education and exp */}
             
             <div className="grid gap-6 md:grid-cols-2">
-                <div>
+                <motion.div variants={fadeInUp} initial="initial" animate="animate">
                     <h5 className="my-3 text-2xl font-blod">Education</h5>
                     <div>
                         <h5 className="my-2 text-xl font-bold">Computer Science Engineering</h5>
                         <p className="font-semibold">University College o1f Engineering(August 2016 - December 2020)</p>
                         <p className="my-3">Complete my <b>B.Tech</b> in Computer Science Engineering</p>
                     </div>
-                </div>
-                <div>
+                </motion.div>
+                <motion.div variants={fadeInUp} initial="initial" animate="animate">
                     <h5 className="my-3 text-2xl font-blod">Experience</h5>
                     <div>
                         <h5 className="my-2 text-xl font-bold">Software Engineer</h5>
                         <p className="font-semibold">Neoito (December 2020 - )</p>
                         <p className="my-3">Working in projects invaolving tech like React, AWS</p>
                     </div>
-                </div>
+                </motion.div>
             </div>
 
             {/* languages and tools */}
